9d0536ae78c6de76ebda20d04f13758d83628d2d
Ferguson230 <37697683+Ferguson230@users.noreply.github.com>
2023-11-19 02:23:25 -0500

    Update secshield.sh

--- secshield.sh
+++ secshield.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "########################"
-echo "########################"
-echo "#####TurnUpSecurity Recaptcha Installation Script"
-echo "#####Maintained by https://turnupsecurityshield.com"
-echo "#####Please email support@turnupsecurityshield.com for any questions regarding this software"
-echo "########################"
-echo "########################"
+echo "################################################################################"
+echo "#                                                                              #"
+echo "#                 TurnUpSecurity Recaptcha Installation Script                 #"
+echo "#                Maintained by https://turnupsecurityshield.com                #"
+echo "#             Please email support@turnupsecurityshield.com for help           #"
+echo "#                                                                              #"
+echo "################################################################################"
 
 # Install Recaptcha v2 server-wide
 cd /usr/local/src && wget -O reCaptcha2_validation-free.tar.gz https://github.com/turnuphosting/reCaptcha2_validation-free/archive/master.tar.gz && sudo tar -zxvf reCaptcha2_validation-free.tar.gz && cd reCaptcha2_validation-free-master/install/ && sudo ./install.sh;
@@ -15,10 +15,10 @@ cd /usr/local/src && wget -O reCaptcha2_validation-free.tar.gz https://github.co
 cd /usr/local/src && rm -rf reCaptcha2_validation* && rm -rf /var/www/html/reCaptcha2_validation-free.tar.gz;
 
 # Congratulations and Welcome Message From TurnUpSecurity Shield
-echo "#####################################################################################################################"
-echo "#####                                                                                                  ##############"
-echo "#####                                                                                                  ##############"
-echo "#####  Congratulations Recaptcha has been successfully installed for your server by TurnUpSecurity.    ##############"
-echo "#####                                                                                                  ##############"
-echo "#####                                                                                                  ##############"
-echo "#####################################################################################################################"
+echo "################################################################################"
+echo "#                                                                              #"
+echo "#                            Congratulations                                   #"
+echo "# Recaptcha has been successfully installed for your server by TurnUpSecurity. #"
+echo "#                                                                              #"
+echo "#                                                                              #"
+echo "################################################################################"
